adf83471198ffa411444659c34d0dfd80489d651
Gyakobo <andygyakobo@gmail.com>
2024-03-19 14:12:24 -0400

    Changed Gitignore

---
+++ app/build/generated/source/buildConfig/debug/com/example/j2375/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.j2375;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.jay_walker_alert_for_watch_os";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
